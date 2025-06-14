4256e5352b91cdf3b2b17aa03dca3879973ff6ac
dianasauval <dianasauval@hotmail.com>
2025-06-14 10:00:37 +0200

    agregando subida de archivos a cloudinary

--- src/services/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "dkdhdy9e5",
-  api_key: "951192131331738",
-  api_secret: "vO0JpRwQuIhAf7QwUkdMuBgrfD8",
-});
-
-export default cloudinary;
